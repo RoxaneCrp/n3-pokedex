56a1af2cf7ec4d481d8dcf32aa43563f0b091499
Roxane Crépeaux <roxane.crepeaux@my-digital-school.org>
2024-05-07 14:41:23 +0200

    Add NavBar

---
+++ components/global/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-center">
+        <ul class="inline-flex  gap-4">
+            <li v-for="item in menuItems" :key="item.name">
+                <NuxtLink :to="item.path" v-slot="{ isActive }">
+                    <div :class="{ 'bg-green-300': isActive }" class="bg-green-100 rounded-md px-4 py-2">
+                        <Icon :name="item.icon" width="32" height="32"  color="black" />
+                        {{ item.name }}
+                    </div>
+                </NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const menuItems = [
+    { name: 'Catalogue', path: '/', icon: 'arcticons:pokemon-tcg-live' },
+    { name: 'Equipe', path: '/team', icon: 'ic:baseline-catching-pokemon' },
+]
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <NavBar class="absolute inset-x-0 bottom-0 mb-5"/>
+    <NuxtPage />
+</template>
+
+<script setup>
+</script>
