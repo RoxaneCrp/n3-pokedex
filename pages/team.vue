34235de3dd8124e8e3fdc8586da9b49649fa64d1
Roxane Crépeaux <roxane.crepeaux@my-digital-school.org>
2024-05-07 16:45:06 +0200

    Add message when empty team and logo

--- pages/team.vue
+++ pages/team.vue
@@ -1,7 +1,13 @@
 <template>
+    <div class="flex justify-center pt-2">
+        <img src="/public/pokedex.png" alt="pokedex logo" class="size-24">
+    </div>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto w-5/6">
-        <div v-for="pokemon in team" :key="pokemon.id" class="bg-white rounded-lg shadow-md overflow-hidden">
+        <div v-if="team.length" v-for="pokemon in team" :key="pokemon.id" class="bg-white rounded-lg shadow-md overflow-hidden">
             <PokeCard :pokemon="pokemon" />
+        </div>
+        <div v-else class="text-center py-4">
+            <p>Aucun Pokémon dans l'équipe.</p>
         </div>
     </div>
 </template>
